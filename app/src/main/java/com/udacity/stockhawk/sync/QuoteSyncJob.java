2224ccd9a8d5c082ad420b35c050fb466f6f516d
Mads Kleemann <madskleemann@me.com>
2017-04-30 17:30:06 +0200

    Added toast when stock symbol wasn’t found

--- app/src/main/java/com/udacity/stockhawk/sync/QuoteSyncJob.java
+++ app/src/main/java/com/udacity/stockhawk/sync/QuoteSyncJob.java
@@ -8,7 +8,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Looper;
+import android.os.Handler;
+import android.widget.Toast;
 
+import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
 import com.udacity.stockhawk.data.PrefUtils;
 
@@ -40,7 +44,7 @@ public final class QuoteSyncJob {
     private QuoteSyncJob() {
     }
 
-    static void getQuotes(Context context) {
+    static void getQuotes(final Context context) {
 
         Timber.d("Running sync job");
 
@@ -69,12 +73,23 @@ public final class QuoteSyncJob {
             ArrayList<ContentValues> quoteCVs = new ArrayList<>();
 
             while (iterator.hasNext()) {
-                String symbol = iterator.next();
+                final String symbol = iterator.next();
 
 
                 Stock stock = quotes.get(symbol);
                 StockQuote quote = stock.getQuote();
-                if (quote.getPrice() == null || quote.getChange() == null || quote.getChangeInPercent() == null) {
+                if (quote == null || quote.getPrice() == null || quote.getChange() == null || quote.getChangeInPercent() == null) {
+                    stockPref.remove(symbol);
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(context,
+                                    context.getString(R.string.toast_stock_not_added_invalid,
+                                            symbol),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
                     continue;
                 }
                 float price = quote.getPrice().floatValue();
